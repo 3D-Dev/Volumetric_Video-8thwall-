8f3e3b07134e9a688cdb65a70d1642f853d93da5
PROCW <57867146+PROCW@users.noreply.github.com>
2021-07-01 18:53:05 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2021 8th Wall, Inc.
+//
+// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
+// is loaded, and before body.html is loaded.
